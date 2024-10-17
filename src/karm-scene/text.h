fb7df3b7749c3f60f432230fd4ba5e0213b1a745
VAN BOSSUYT Nicolas <me@smnx.sh>
2024-10-17 13:18:38 +0200

    karm-text: Removed the Font from Run.

--- src/karm-scene/text.h
+++ src/karm-scene/text.h
@@ -8,16 +8,17 @@ namespace Karm::Scene {
 
 struct Text : public Node {
     Math::Vec2f baseline;
-    Strong<Karm::Text::Run> run;
+    Karm::Text::Font font;
+    Karm::Text::Run run;
     Gfx::Fill fill;
 
-    Text(Math::Vec2f baseline, Strong<Karm::Text::Run> run, Gfx::Fill fill)
-        : baseline(baseline), run(run), fill(fill) {}
+    Text(Math::Vec2f baseline, Karm::Text::Font font, Karm::Text::Run run, Gfx::Fill fill)
+        : baseline(baseline), font(font), run(run), fill(fill) {}
 
     void paint(Gfx::Canvas &g) override {
         g.push();
         g.fillStyle(fill);
-        g.fill(run->_font, *run, baseline);
+        g.fill(font, run, baseline);
 
         // g.beginPath();
         // g.line(Math::Edgef{baseline, baseline + Math::Vec2f{run->_width, 0}});
