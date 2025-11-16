c495be72a2a9df1167199c4e1a761994bd66c464
sleepy-monax <me@smnx.sh>
2025-11-16 18:46:36 +0100

    meta: Bump ce-libc and remived f16 again.

--- src/karm-core/base/base.cpp
+++ src/karm-core/base/base.cpp
@@ -127,9 +127,10 @@ export using i128 = __int128_t;
 
 // MARK: Floating point --------------------------------------------------------
 
-#ifdef __FLT16_MANT_DIG__
-export using f16 = _Float16;
-#endif
+// NOTE: LLVM has limited support for _Float16 and requires runtime library support
+// #ifdef __FLT16_MANT_DIG__
+// export using f16 = _Float16;
+// #endif
 
 export using f32 = float;
 export using f64 = double;
