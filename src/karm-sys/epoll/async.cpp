05244cfc4070eda0b7745da225067ffd431cc542
sleepy-monax <me@smnx.sh>
2025-12-18 11:33:27 +0100

    karm-sys: Added support for epoll cancelation.

--- src/karm-sys/epoll/async.cpp
+++ src/karm-sys/epoll/async.cpp
@@ -18,7 +18,7 @@ namespace Karm::Sys::_Embed {
 struct EpollSched : Sys::Sched {
     int _epollFd;
     usize _id = 0;
-    Map<usize, Async::Promise<>> _promises;
+    Map<usize, Async::Promise<u32>> _promises;
 
     EpollSched(int epollFd)
         : _epollFd(epollFd) {}
@@ -28,9 +28,9 @@ struct EpollSched : Sys::Sched {
     }
 
     [[clang::coro_wrapper]]
-    Async::Task<> waitFor(epoll_event ev, int fd) {
+    Async::Task<u32> waitFor(epoll_event ev, int fd) {
         usize id = _id++;
-        auto promise = Async::Promise<>();
+        auto promise = Async::Promise<u32>();
         auto future = promise.future();
 
         ev.data.u64 = id;
@@ -39,7 +39,7 @@ struct EpollSched : Sys::Sched {
         struct stat st{};
         if (fstat(fd, &st) == 0 && (S_ISREG(st.st_mode) || S_ISDIR(st.st_mode))) {
             // Treat as immediately "ready" for our purposes.
-            return Async::makeTask(Async::One<Res<>>{Ok()});
+            return Async::makeTask(Async::One<Res<u32>>{Ok(ev.events)});
         }
 
         // Try to add to epoll
@@ -50,58 +50,84 @@ struct EpollSched : Sys::Sched {
                     _promises.put(id, std::move(promise));
                     return Async::makeTask(future);
                 }
-                return Async::makeTask(Async::One<Res<>>{Posix::fromLastErrno()});
+                return Async::makeTask(Async::One<Res<u32>>{Posix::fromLastErrno()});
             }
 
             if (errno == EPERM) {
                 // Not epollable (e.g., regular file, some character devices). Consider it ready.
-                return Async::makeTask(Async::One<Res<>>{Ok()});
+                return Async::makeTask(Async::One<Res<u32>>{Ok(ev.events)});
             }
 
-            return Async::makeTask(Async::One<Res<>>{Posix::fromLastErrno()});
+            return Async::makeTask(Async::One<Res<u32>>{Posix::fromLastErrno()});
         }
 
         _promises.put(id, std::move(promise));
         return Async::makeTask(future);
     }
 
-    Async::Task<usize> readAsync(Rc<Fd> fd, MutBytes buf) override {
+    Async::Task<usize> readAsync(Rc<Fd> fd, MutBytes buf, Async::CancellationToken ct) override {
         int rawFd = co_try$(Posix::toPosixFd(fd))->_raw;
         co_trya$(waitFor({.events = EPOLLIN | EPOLLET, .data = {}}, rawFd));
+        co_try$(ct.errorIfCanceled());
         co_return Ok(co_try$(fd->read(buf)));
     }
 
-    Async::Task<usize> writeAsync(Rc<Fd> fd, Bytes buf) override {
+    Async::Task<usize> writeAsync(Rc<Fd> fd, Bytes buf, Async::CancellationToken ct) override {
         int rawFd = co_try$(Posix::toPosixFd(fd))->_raw;
         co_trya$(waitFor({.events = EPOLLOUT | EPOLLET, .data = {}}, rawFd));
+        co_try$(ct.errorIfCanceled());
         co_return Ok(co_try$(fd->write(buf)));
     }
 
-    Async::Task<> flushAsync(Rc<Fd> fd) override {
+    Async::Task<> flushAsync(Rc<Fd> fd, Async::CancellationToken ct) override {
         int rawFd = co_try$(Posix::toPosixFd(fd))->_raw;
         co_trya$(waitFor({.events = EPOLLOUT | EPOLLET, .data = {}}, rawFd));
+        co_try$(ct.errorIfCanceled());
         co_return Ok(co_try$(fd->flush()));
     }
 
-    Async::Task<_Accepted> acceptAsync(Rc<Fd> fd) override {
+    Async::Task<_Accepted> acceptAsync(Rc<Fd> fd, Async::CancellationToken ct) override {
         int rawFd = co_try$(Posix::toPosixFd(fd))->_raw;
         co_trya$(waitFor({.events = EPOLLIN | EPOLLET, .data = {}}, rawFd));
+        co_try$(ct.errorIfCanceled());
         co_return Ok(co_try$(fd->accept()));
     }
 
-    Async::Task<_Sent> sendAsync(Rc<Fd> fd, Bytes buf, Slice<Handle> handles, SocketAddr addr) override {
+    Async::Task<_Sent> sendAsync(Rc<Fd> fd, Bytes buf, Slice<Handle> handles, SocketAddr addr, Async::CancellationToken ct) override {
         int rawFd = co_try$(Posix::toPosixFd(fd))->_raw;
         co_trya$(waitFor({.events = EPOLLOUT | EPOLLET, .data = {}}, rawFd));
+        co_try$(ct.errorIfCanceled());
         co_return Ok(co_try$(fd->send(buf, handles, addr)));
     }
 
-    Async::Task<_Received> recvAsync(Rc<Fd> fd, MutBytes buf, MutSlice<Handle> hnds) override {
+    Async::Task<_Received> recvAsync(Rc<Fd> fd, MutBytes buf, MutSlice<Handle> hnds, Async::CancellationToken ct) override {
         int rawFd = co_try$(Posix::toPosixFd(fd))->_raw;
         co_trya$(waitFor({.events = EPOLLIN | EPOLLET, .data = {}}, rawFd));
+        co_try$(ct.errorIfCanceled());
         co_return Ok(co_try$(fd->recv(buf, hnds)));
     }
 
-    Async::Task<> sleepAsync(Instant until) override {
+    Async::Task<Flags<Poll>> pollAsync(Rc<Fd> fd, Flags<Poll> events, Async::CancellationToken ct) override {
+        unsigned pollMask = 0;
+        if (events.has(Poll::READABLE))
+            pollMask |= EPOLLIN;
+        if (events.has(Poll::WRITEABLE))
+            pollMask |= EPOLLOUT;
+
+        int rawFd = co_try$(Posix::toPosixFd(fd))->_raw;
+
+        co_try$(ct.errorIfCanceled());
+        auto res = co_trya$(waitFor({.events = pollMask | EPOLLET, .data = {}}, rawFd));
+
+        events = {};
+        if (res & EPOLLIN)
+            events.set(Poll::READABLE);
+        if (res & EPOLLOUT)
+            events.set(Poll::READABLE);
+        co_return Ok(events);
+    }
+
+    Async::Task<> sleepAsync(Instant until, Async::CancellationToken ct) override {
         Instant instant = Sys::instant();
         Duration delta = Duration::zero();
         if (instant < until)
@@ -119,6 +145,7 @@ struct EpollSched : Sys::Sched {
         if (timerfd_settime(timeFd, 0, &spec, nullptr) < 0)
             co_return Posix::fromLastErrno();
 
+        co_try$(ct.errorIfCanceled());
         co_trya$(waitFor({.events = EPOLLIN, .data = {}}, timeFd));
 
         co_return Ok();
@@ -142,7 +169,7 @@ struct EpollSched : Sys::Sched {
 
         usize id = ev.data.u64;
         auto promise = _promises.take(id);
-        promise.resolve(Ok());
+        promise.resolve(Ok(ev.events));
         return Ok();
     }
 };
