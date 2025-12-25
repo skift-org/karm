0946b922c0fe9d3491abc0bcd27bf70d8e2ee712
sleepy-monax <nicolas.van.bossuyt@gmail.com>
2025-12-25 15:32:01 +0000

    Deploying to gh-pages from @ skift-org/karm@a61fcb40f1359515f61435a27ee029d5c1b2c489 🚀

--- structKarm_1_1Async_1_1Queue.js
+++ structKarm_1_1Async_1_1Queue.js
@@ -1,9 +1,9 @@
 var structKarm_1_1Async_1_1Queue =
 [
     [ "Queue", "structKarm_1_1Async_1_1Queue.html#a677a987f17f8f23a07195ed97bcfe1d9", null ],
-    [ "dequeue", "structKarm_1_1Async_1_1Queue.html#a5f26bb43caac8bd0df0cf94313ee7b21", null ],
     [ "dequeueAsync", "structKarm_1_1Async_1_1Queue.html#a82f5b0479339d61ec264a95770c258cd", null ],
     [ "emplace", "structKarm_1_1Async_1_1Queue.html#a54750f1a4eed9173d3b92eb3d000c79f", null ],
     [ "empty", "structKarm_1_1Async_1_1Queue.html#a6b30dc75ff278f7276c4f16290492172", null ],
-    [ "enqueue", "structKarm_1_1Async_1_1Queue.html#ad3e7bcde5a3c08eb91d372a9bb9fa260", null ]
+    [ "enqueue", "structKarm_1_1Async_1_1Queue.html#ad3e7bcde5a3c08eb91d372a9bb9fa260", null ],
+    [ "tryDequeue", "structKarm_1_1Async_1_1Queue.html#a43f2d4d68429b9ae35c7c822603aac3f", null ]
 ];
