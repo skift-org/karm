ecef58cb150e5482e3f9ebc2c1371bca862791e9
sleepy-monax <me@smnx.sh>
2026-04-08 16:33:27 +0200

    karm-icu: Added minimal implementation of countGrapheneClusters.

--- src/karm-core/icu/segment.cpp
+++ src/karm-core/icu/segment.cpp
@@ -1,8 +1,17 @@
 export module Karm.Core:icu.segment;
 
+import :base.string;
+
 namespace Karm::Icu {
 
 // Implementation of the unicode text segmentation algorithm
 // https://unicode.org/reports/tr29/
+export usize countGrapheneClusters(Str s) {
+    // FIXME: This is not how you do that 🤪
+    usize n = 0;
+    for (auto _ : iterRunes(s))
+        n++;
+    return n;
+}
 
 } // namespace Karm::Icu
