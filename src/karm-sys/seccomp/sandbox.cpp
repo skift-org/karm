94f64bc32bc4285e687da68c4f79de5d9d8fd535
sleepy-monax <me@smnx.sh>
2026-04-09 11:10:22 +0200

    karm-sys: Hardened the sandbox.

--- src/karm-sys/seccomp/sandbox.cpp
+++ src/karm-sys/seccomp/sandbox.cpp
@@ -1,6 +1,12 @@
 module;
 
+#include <fcntl.h>
+#include <karm/macros>
+#include <linux/landlock.h>
 #include <seccomp.h>
+#include <sys/prctl.h>
+#include <sys/resource.h>
+#include <unistd.h>
 
 module Karm.Sys;
 
@@ -32,9 +38,56 @@ namespace Karm::Sys::_Embed {
     SYSCALL(read)                 \
     SYSCALL(write)
 
-Res<> hardenSandbox() {
-    scmp_filter_ctx ctx = seccomp_init(SCMP_ACT_KILL_PROCESS);
+static Res<> _landlockInDirectory(Str dir) {
+    landlock_ruleset_attr attr = {};
+    attr.handled_access_fs = LANDLOCK_ACCESS_FS_READ_FILE | LANDLOCK_ACCESS_FS_WRITE_FILE;
 
+    int rulesetFd = syscall(__NR_landlock_create_ruleset, &attr, sizeof(attr), 0);
+    if (rulesetFd < 0)
+        return Posix::fromLastErrno();
+
+    Defer closeRuleset{[&] {
+        close(rulesetFd);
+    }};
+
+    int dirFd = open(dir.buf(), O_PATH | O_DIRECTORY | O_CLOEXEC);
+    if (dirFd < 0)
+        return Posix::fromLastErrno();
+
+    Defer closeDir{[&] {
+        close(dirFd);
+    }};
+
+    landlock_path_beneath_attr pathAttr = {
+        .allowed_access = attr.handled_access_fs,
+        .parent_fd = dirFd
+    };
+
+    if (syscall(__NR_landlock_add_rule, rulesetFd, LANDLOCK_RULE_PATH_BENEATH, &pathAttr, 0) < 0)
+        return Posix::fromLastErrno();
+
+    if (syscall(__NR_landlock_restrict_self, rulesetFd, 0) < 0)
+        return Posix::fromLastErrno();
+
+    return Ok();
+}
+
+Res<> hardenSandbox() {
+    // MARK: RLimit
+    constexpr rlimit rl{.rlim_cur = 4_GiB, .rlim_max = 4_GiB};
+    if (setrlimit(RLIMIT_AS, &rl) < 0)
+        return Posix::fromLastErrno();
+
+    // MARK: Landlock
+    auto [repo, _] = try$(Posix::repoRoot());
+    try$(_landlockInDirectory(repo));
+
+    // https://www.kernel.org/doc/Documentation/prctl/no_new_privs.txt
+    if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) < 0)
+        return Posix::fromLastErrno();
+
+    // MARK: Seccomp
+    scmp_filter_ctx ctx = seccomp_init(SCMP_ACT_KILL_PROCESS);
     if (!ctx)
         return Posix::fromLastErrno();
 
@@ -42,10 +95,10 @@ Res<> hardenSandbox() {
         seccomp_release(ctx);
     }};
 
-#define ITER(SYSCALL)                                                                    \
-    if (auto it = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(SYSCALL), 0); it < 0) { \
-        return Posix::fromErrno(-it);                                                    \
-    }
+#define ITER(SYSCALL)                                                                  \
+    if (auto it = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(SYSCALL), 0); it < 0) \
+        return Posix::fromErrno(-it);
+
     FOREACH_SYSCALLS(ITER)
 #undef ITER
 
