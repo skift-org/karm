56d391af382bb96392e3e7fbbb1b883e3b2f8527
sleepy-monax <me@smnx.sh>
2026-09-03 00:16:25 +0200

    karm-base: Add counting bloom.

--- src/karm-core/base/bloom.cpp
+++ src/karm-core/base/bloom.cpp
@@ -11,11 +11,27 @@ using namespace Karm::Literals;
 
 namespace Karm {
 
+template <typename V>
+Pair<u64, u64> _hashItemPair(V const& value) {
+    DefaultHasher hasher;
+    Karm::hash(hasher, value);
+
+    u64 h = hasher.finish();
+
+    // NOTE: The hash quality of DefaultHasher might not be
+    //       the same on the high bits and low bits so
+    //       we scramble using fmix64 from MurmurHash3.
+    h ^= h >> 33;
+    h *= 0xff51afd7ed558ccd;
+    h ^= h >> 33;
+    h *= 0xc4ceb9fe1a85ec53;
+    h ^= h >> 33;
+
+    return {h & 0xffffffff, h >> 32};
+}
+
 export template <typename T>
 struct Bloom {
-    static constexpr u64 SEED1 = 0x9e3779b97f4a7c15;
-    static constexpr u64 SEED2 = 0x517cc1b727220a95;
-
     Vec<u8> _buf;
     usize _k;
 
@@ -34,21 +50,9 @@ struct Bloom {
         _buf.resize(size);
     }
 
-    Pair<u64, u64> _hashPair(Meta::Equatable<T> auto const& value) const {
-        DefaultHasher hasher1;
-        Karm::hash(hasher1, SEED1);
-        Karm::hash(hasher1, value);
-
-        DefaultHasher hasher2;
-        Karm::hash(hasher2, SEED2);
-        Karm::hash(hasher2, value);
-
-        return {hasher1.finish(), hasher2.finish()};
-    }
-
     void add(Meta::Equatable<T> auto const& value) {
         MutBits bits{_buf};
-        auto [h1, h2] = _hashPair(value);
+        auto [h1, h2] = _hashItemPair(value);
         usize m = bits.len();
 
         // https://en.wikipedia.org/wiki/Double_hashing
@@ -60,7 +64,7 @@ struct Bloom {
 
     bool maybeContains(Meta::Equatable<T> auto const& value) const {
         Bits bits{_buf};
-        auto [h1, h2] = _hashPair(value);
+        auto [h1, h2] = _hashItemPair(value);
         usize m = bits.len();
         for (usize i : urange::zeroTo(_k)) {
             // https://en.wikipedia.org/wiki/Double_hashing
@@ -73,6 +77,50 @@ struct Bloom {
 
     void clear() {
         zeroFill(mutBytes(_buf));
+    }
+};
+
+/// Bloom filter that supports removal, but is less space efficient than the normal one.
+export template <typename T, usize N = 4096>
+struct CountingBloom {
+    static_assert((N & (N - 1)) == 0, "CountingBloom size must be a power of two");
+
+    static constexpr usize MASK = N - 1;
+    static constexpr u8 SATURATED = ~u8{0};
+
+    Array<u8, N> _slots = {};
+
+    void _increment(usize slot) {
+        if (_slots[slot] != SATURATED)
+            ++_slots[slot];
+    }
+
+    void _decrement(usize slot) {
+        // NOTE: Once a slot is saturated, we cannot remove from it anymore.
+        if (_slots[slot] != SATURATED)
+            --_slots[slot];
+    }
+
+    void add(Meta::Equatable<T> auto const& value) {
+        auto [h1, h2] = _hashItemPair(value);
+        _increment(h1 & MASK);
+        _increment(h2 & MASK);
+    }
+
+    void remove(Meta::Equatable<T> auto const& value) {
+        auto [h1, h2] = _hashItemPair(value);
+        _decrement(h1 & MASK);
+        _decrement(h2 & MASK);
+    }
+
+    bool maybeContains(Meta::Equatable<T> auto const& value) const {
+        auto [h1, h2] = _hashItemPair(value);
+        return _slots[h1 & MASK] and _slots[h2 & MASK];
+    }
+
+    void clear() {
+        for (usize i = 0; i < N; i++)
+            _slots[i] = 0;
     }
 };
 
--- src/karm-core/base/tests/test-bloom.cpp
+++ src/karm-core/base/tests/test-bloom.cpp
@@ -15,4 +15,44 @@ test$("bloom") {
     return Ok();
 }
 
+test$("counting-bloom-add-and-remove") {
+    CountingBloom<u64> bloom;
+
+    expect$(not bloom.maybeContains(0xabc));
+
+    bloom.add(0xabc);
+    expect$(bloom.maybeContains(0xabc));
+
+    bloom.remove(0xabc);
+    expect$(not bloom.maybeContains(0xabc));
+
+    return Ok();
+}
+
+test$("counting-bloom-nests") {
+    CountingBloom<u64> bloom;
+
+    bloom.add(0x1234);
+    bloom.add(0x1234);
+    bloom.remove(0x1234);
+    expect$(bloom.maybeContains(0x1234));
+
+    bloom.remove(0x1234);
+    expect$(not bloom.maybeContains(0x1234));
+
+    return Ok();
+}
+
+test$("counting-bloom-never-false-negative") {
+    CountingBloom<u64, 64> bloom;
+
+    for (u64 i = 0; i < 200; i++)
+        bloom.add(i);
+
+    for (u64 i = 0; i < 200; i++)
+        expect$(bloom.maybeContains(i));
+
+    return Ok();
+}
+
 } // namespace Karm::Base::Tests
